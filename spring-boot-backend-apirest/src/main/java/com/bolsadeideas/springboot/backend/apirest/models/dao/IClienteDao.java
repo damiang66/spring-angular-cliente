21a50cefcb8add029b08191f8ac89ec71c3cb536
damian <adamodamian6@gmail.com>
2023-01-03 22:40:38 -0300

    primer commit

---
+++ spring-boot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/models/dao/IClienteDao.java
@@ -0,0 +1,10 @@
+package com.bolsadeideas.springboot.backend.apirest.models.dao;
+
+
+
+import com.bolsadeideas.springboot.backend.apirest.models.entity.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IClienteDao extends JpaRepository<Cliente, Long>{
+
+}
